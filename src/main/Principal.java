bc1c3cd6436f75c600cc54ce3686d6e89ade8238
AHTepsilon <alehertoro725@gmail.com>
2021-10-14 13:26:26 -0500

    initialized the UDP peer

---
+++ src/main/Principal.java
@@ -0,0 +1,43 @@
+package main;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import processing.core.PApplet;
+
+public class Principal extends PApplet
+{
+
+	public static void main(String[] args) 
+	{
+		PApplet.main("main.Principal");
+	}
+	
+	InetAddress ip;
+	
+	@Override
+	public void settings() //void Awake
+	{
+		size(500, 500);
+	}
+	
+	@Override
+	public void setup() //void Start
+	{
+		try {
+			ip = InetAddress.getLocalHost();
+			
+			System.out.println(ip.getHostAddress());
+			System.out.println(ip.getHostName());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void draw() //void Update
+	{		
+
+	}
+
+}
---
+++ src/main/UDPConnection.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UDPConnection extends Thread 
+{
+
+	private DatagramSocket socket;
+	
+	public void run()
+	{
+		try {
+			socket = new DatagramSocket(2000);
+			
+			
+			while(true)
+			{
+				byte[] buffer = new byte[100];
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				System.out.println("Awaiting for datagram...");
+				socket.receive(packet);
+				
+				String message = new String(packet.getData()).trim();
+				System.out.println("Datagram Received: ");
+				
+			}
+			
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(String msg)
+	{
+		InetAddress ip;
+		try {
+			ip = InetAddress.getByName("127.0.0.1");
+			DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.getBytes().length, ip, 2000);
+			socket.send(packet);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
